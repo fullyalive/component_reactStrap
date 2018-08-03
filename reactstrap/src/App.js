0f100f3abcb6fc739cb301a4597f2c285cc6eb53
fullyalive <wogud1202@gmail.com>
2018-08-03 14:08:38 +0900

    Carousel

--- reactstrap/src/App.js
+++ reactstrap/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import DropdownButton from "./Dropdown/DropdownMenu";
+import CarouselSlider from "./Carousel/CarouselSlider";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <DropdownButton />
+        <CarouselSlider />
       </div>
     );
   }
